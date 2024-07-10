a679d52481291bf9bcb33da55051c934334c96aa
Aimee <AimeeAngelia2022@Gmail.com>
2024-07-10 23:07:30 +0800

    Update Normally
    # C语言配置完成
    # (伟大胜利✌️)

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+       printf("Hello, World!\n");         // This is a simple C program that prints "Hello, World!" to the console.
+       return 0;                    // The return statement is used to exit the main function and return a value to the operating system. In this case, the value is 0, which indicates that the program executed successfully.
+}
